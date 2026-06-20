1408a3355de84aa94762f5cd8625bc1ccdc3c23f
vijayaragavan-r <ragavan7032007@gmail.com>
2026-06-20 13:47:17 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class Main {
+class Reverse {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int num = sc.nextInt();
