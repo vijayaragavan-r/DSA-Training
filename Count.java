4cb8c266b21629bbe334cc2c4d12c09eb4d77266
Vijayaragavan <ragavan7032007@gmail.com>
2026-06-20 13:57:31 +0530

    Initial commit

---
+++ Count.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class Count {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        int count = 0;
+        while (num > 0) {
+            count++;
+            num = num / 10;
+        }
+        System.out.println(count);
+    }
+}
